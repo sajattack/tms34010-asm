ff26f13b045507aa512f4f3ccf34b2e66afea51c
Paul Sajna <hello@paulsajna.com>
2021-07-17 14:30:36 -0700

    all instructions enumerated

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-mod instruction;
-mod symbol;
+pub mod instruction;
+pub mod symbol;
 
 fn main() {
     println!("Hello, world!");
--- src/symbol.rs
+++ src/symbol.rs
@@ -1,6 +1,27 @@
+#[derive(Debug, Clone, Copy)]
 pub struct Rs(u8);
+#[derive(Debug, Clone, Copy)]
 pub struct Rd(u8);
+#[derive(Debug, Clone, Copy)]
 pub struct IW(u16);
+#[derive(Debug, Clone, Copy)]
 pub struct IL(u32);
+#[derive(Debug, Clone, Copy)]
 pub struct K(u8);
+#[derive(Debug, Clone, Copy)]
 pub struct F(u8);
+#[derive(Debug, Clone, Copy)]
+pub struct Address(u32); // 32-bit specified in assembly code, turned into 8-bit offset from current PC
+#[derive(Debug, Clone, Copy)]
+pub struct FS(u8);
+#[derive(Debug, Clone, Copy)]
+pub struct FE(u8);
+#[derive(Debug, Clone, Copy)]
+pub struct N(u8);
+#[derive(Debug, Clone, Copy)]
+pub struct Offset(u8);
+#[derive(Debug, Clone, Copy)]
+pub struct Z(u8);
+#[derive(Debug, Clone, Copy)]
+pub struct Condition(u8);
+
